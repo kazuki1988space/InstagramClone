d811427b5631afb3b91c0541213d6e5ebc829370
kazuki <neworder1988@i.spftbank.jp>
2018-03-20 00:58:12 +0900

    repair user_edit.rb

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -3,6 +3,6 @@ class ContactMailer < ApplicationMailer
   def contact_mail(picture)
     @picture = picture
 
-    mail to: @picture.user.email, subject: "お問い合わせの確認メール"
+    mail to: @picture.user.email, subject: "投稿確認メール"
   end
 end
